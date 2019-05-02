ff6eadf20d6e16bb8572305f8e4d3f53550ec93d
Reno Sorensen <renojames19@gmail.com>
2019-05-02 04:13:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -32,7 +32,7 @@ class Application
       search_term = req.params["q"]
 
       if @@items.include?(search_term)
-        resp.write "Added #{search_term}"
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
